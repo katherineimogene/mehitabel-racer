aec2e75ee37622c4c21f8cd202d426ae112ab782
katherineimogene <katherineimogene@gmail.com>
2014-03-26 00:01:03 -0700

    Changes appearance of letters as user types correctly.   * counts mistakes   * prompts next letter if consecutive misses is >= 4.

--- typeracer.js
+++ typeracer.js
@@ -1,24 +1,45 @@
+window.onload=function(){
+  gameTextDiv = document.getElementById('game-text')
+  gameText = gameTextDiv.innerText
+  gameTextArr = gameText.split("")
+  readyDom()
+  missedCharDiv = document.getElementById('missed-chars')
+}
 
-  typeRace = function(e){
-    var gameTextDiv = document.getElementById('game-text')
-    var gameText = gameTextDiv.innerText
-    var gameTextArr = gameText.split("")
-
-    gameTextDiv.innerHTML = ""
-    for (i=0; i<gameTextArr.length; i++){
-      newSpan = document.createElement('span')
-      newSpan.innerText = gameTextArr[i]
-      gameTextDiv.appendChild(newSpan)
-    }
-
-    for (i=0; i<gameTextArr.length; i++){
-      var input = String.fromCharCode(e.keyCode || e.charCode)
-      var toMatch = gameTextArr[i]
-      var currentSpan = gameTextDiv.children[i]
-      if (input.downcase == gameTextArr[i].downcase) {
-        currentSpan.classList.add('match')
-      }
-    }
+readyDom = function() {
+   gameTextDiv.innerHTML = ""
+  for (i=0; i<gameTextArr.length; i++){
+    newSpan = document.createElement('span')
+    newSpan.innerText = gameTextArr[i]
+    gameTextDiv.appendChild(newSpan)
   }
+}
 
-document.addEventListener('keydown', typeRace, false)
+captureInput = function(e) {
+  return String.fromCharCode(e.keyCode || e.charCode)
+}
+
+counter = 0
+incorrect = 0
+tempMissed = 0
+
+typeRace = function(e) {
+    e.preventDefault()
+    if (captureInput(e).toLowerCase() == gameTextArr[counter].toLowerCase()) {
+      tempMissed = 0
+      missedCharDiv.children[1].innerHTML = ""
+      gameTextDiv.children[counter].classList.add('match')
+      counter += 1
+    } else {
+    tempMissed += 1
+    if (tempMissed >= 4) {
+      missedCharDiv.children[1].innerHTML = gameTextArr[counter]
+    }
+    incorrect += 1
+    missedCharDiv.children[0].innerHTML = incorrect
+
+    }
+}
+
+document.addEventListener('keydown', function(e){
+  typeRace(e)} , false)
