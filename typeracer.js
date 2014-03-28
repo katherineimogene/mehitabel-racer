4e00045640be739854e1254d20819f0ae4ebf48e
katherineimogene <katherineimogene@gmail.com>
2014-03-28 16:13:53 -0700

    OO refactor

--- typeracer.js
+++ typeracer.js
@@ -1,49 +1,113 @@
 window.onload=function(){
-  gameTextDiv = document.getElementById('game-text')
-  gameText = gameTextDiv.innerText
-  gameTextArr = gameText.split("")
-  readyDom()
-  missedCharDiv = document.getElementById('missed-chars')
+  var view = new View()
+  var game = new Game()
+  var controller = new Controller(view,game)
+  view.readyDom()
+  document.addEventListener('keydown', function() { controller.keyDownEvent(event) } , false)
 }
 
-readyDom = function() {
-   gameTextDiv.innerHTML = ""
-  for (i=0; i<gameTextArr.length; i++){
-    newSpan = document.createElement('span')
-    newSpan.innerText = gameTextArr[i]
-    gameTextDiv.appendChild(newSpan)
+var View = function(){
+  this.gameTextDiv = document.getElementById('game-text')
+  this.gameText = this.gameTextDiv.innerText
+  this.gameTextArr = this.gameText.split("")
+  this.missedCharDiv = document.getElementById('missed-chars')
+}
+
+View.prototype = {
+  readyDom: function(){
+    this.gameTextDiv.innerHTML = ""
+    for (i=0; i<this.gameTextArr.length; i++){
+      newSpan = document.createElement('span')
+      newSpan.innerText = this.gameTextArr[i]
+      this.gameTextDiv.appendChild(newSpan)
+    }
+  },
+  resetMissedCharText: function(){
+    this.missedCharDiv.children[1].innerHTML = ""
+  },
+
+  isSpace: function(index){
+    return this.gameTextArr[index] == " "
+  },
+
+
+  showHint: function(index){
+    if ( this.isSpace(index) ) {
+      charHint = "space"
+    } else {
+      charHint = this.gameTextArr[index]
+    }
+    this.missedCharDiv.children[1].innerHTML = charHint
+  },
+
+  showIncorrect: function(numIncorrect){
+    this.missedCharDiv.children[0].innerHTML = numIncorrect
+  },
+
+  expectedChar: function(index) {
+    return this.gameTextArr[index]
+  },
+
+  addMatch: function(index){
+      this.gameTextDiv.children[index].classList.add('match')
   }
 }
 
-captureInput = function(e) {
-  return String.fromCharCode(e.keyCode || e.charCode)
+Controller = function(view,game) {
+  this.view = view
+  this.game = game
 }
 
-counter = 0
-incorrect = 0
-tempMissed = 0
+Controller.prototype = {
 
-typeRace = function(e) {
+  captureInput: function(e) {
+    return String.fromCharCode(e.keyCode || e.charCode)
+  },
+
+  rightEvent:  function(){
+    this.resetMissedChar()
+    this.view.addMatch(this.game.letterIndex)
+    this.game.updateCounter("letterIndex")
+  },
+
+  resetMissedChar: function(){
+    this.game.tempMissed = 0
+    this.view.resetMissedCharText()
+  },
+
+  wrongEvent: function() {
+    this.game.updateCounter("tempMissed")
+    if (this.game.tempMissed >= 2) {
+      this.view.showHint(this.game.letterIndex)
+    }
+    this.game.updateCounter("incorrect")
+    this.view.showIncorrect(this.game.incorrect)
+  },
+
+  correctInput: function(e){
+    return this.captureInput(e).toLowerCase() == this.view.expectedChar(this.game.letterIndex)
+  },
+
+  keyDownEvent: function(e) {
     e.preventDefault()
-    if (captureInput(e).toLowerCase() == gameTextArr[counter].toLowerCase()) {
-      tempMissed = 0
-      missedCharDiv.children[1].innerHTML = ""
-      gameTextDiv.children[counter].classList.add('match')
-      counter += 1
-    } else {
-    tempMissed += 1
-    if (tempMissed >= 2) {
-      if ( gameTextArr[counter] == " " ) { charHint = "space" }
-      else {
-        charHint = gameTextArr[counter]
-      }
-      missedCharDiv.children[1].innerHTML = charHint
-    }
-    incorrect += 1
-    missedCharDiv.children[0].innerHTML = incorrect
-
-    }
+    this.correctInput(e) ? this.rightEvent() : this.wrongEvent()
+  }
 }
 
-document.addEventListener('keydown', function(e){
-  typeRace(e)} , false)
+Game = function() {
+  this.letterIndex = 0
+  this.incorrect = 0
+  this.tempMissed = 0
+}
+
+Game.prototype = {
+  updateCounter: function( type ){
+    this[type] += 1
+  }
+}
+
+
+
+
+
+
